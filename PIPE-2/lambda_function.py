b0e834c172d1c7e2e9689e83ea7c18562e1ae7b0
gkanakaws <58528839+gkanakaws@users.noreply.github.com>
2021-05-15 22:11:31 -0700

    lambdafunc inside folder

---
+++ PIPE-2/lambda_function.py
@@ -0,0 +1,39 @@
+def lambda_handler(event, context):
+    import json
+    modifiedFiles = event["commits"][0]["modified"]
+    #full path
+    for filePath in modifiedFiles:
+        # Extract folder name
+        folderName = (filePath[:filePath.find("/")])
+        print(folderName)
+        break
+
+    if len(folderName)>0:
+        # Codepipeline name is foldername-job. 
+        # We can read the configuration from S3 as well. 
+        returnCode = start_code_pipeline(folderName + '-job')
+        returnCode = start_code_pipeline('github-codepipeline-CodePipeline-17UZ9KMX6IQHL')
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Modified project in repo::' + folderName)
+    }
+    
+
+def start_code_pipeline(pipelineName):
+    client = codepipeline_client()
+    response = client.start_pipeline_execution(name=pipelineName)
+    return True
+
+cpclient = None
+def codepipeline_client():
+    import boto3
+    global cpclient
+    if not cpclient:
+        cpclient = boto3.client('codepipeline')
+    return cpclient
+   
+
+# https://gitpython.readthedocs.io/en/stable/tutorial.html
+
+# https://aws.amazon.com/blogs/devops/integrate-github-monorepo-with-aws-codepipeline-to-run-project-specific-ci-cd-pipelines/
