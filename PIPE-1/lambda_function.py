8021db7e3f22244f84148c3cc8216753a0d5894a
gkanakaws <58528839+gkanakaws@users.noreply.github.com>
2021-05-15 22:57:07 -0700

    Update lambda_function.py

--- PIPE-1/lambda_function.py
+++ PIPE-1/lambda_function.py
@@ -4,28 +4,28 @@ cplient = boto3.client('codepipeline')
 
 def lambda_handler(event, context):
     
-    modifiedFiles = event["commits"][0]["modified"]
+#    modifiedFiles = event["commits"][0]["modified"]
     #returnCode = start_code_pipeline(PIPE-1)
     #full path--
-    for filePath in modifiedFiles:
+#    for filePath in modifiedFiles:
         # Extract folder name
-        folderName = (filePath[:filePath.find("/")])
-        print(folderName)
-        break
+#        folderName = (filePath[:filePath.find("/")])
+#        print(folderName)
+#        break
 
-    if len(folderName)>0:
+#    if len(folderName)>0:
         # Codepipeline name is foldername-job. 
         # We can read the configuration from S3 as well. 
         #returnCode = start_code_pipeline(folderName + '-job')
         #returnCode = start_code_pipeline('github-codepipeline-CodePipeline-17UZ9KMX6IQHL')
         #returnCode = start_code_pipeline(PIPE-1)
 
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Modified project in repo:' + folderName)
-    }
+#    return {
+#        'statusCode': 200,
+#        'body': json.dumps('Modified project in repo:' + folderName)
+#    }
     
-    cpresponse = cp_client.start_pipeline_execution(name=PIPE-1)
+    cpresponse = cpclient.start_pipeline_execution(name=PIPE-1)
                 
 ##def start_code_pipeline(pipelineName):
 #    client = codepipeline_client()
